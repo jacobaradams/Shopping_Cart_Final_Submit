17d05b07fd998bb935dac1ec5baf803668e135cb
Jacob Adams <jacob.adams@csuglobal.edu>
2024-08-11 18:55:42 -0500

    Trying print statement on Item1

--- cost_calculator.py
+++ cost_calculator.py
@@ -5,4 +5,6 @@ class ItemToPurchase:
         self.item_quantity = int(item_quantity)
     def print_item_cost(self):
         item_total_cost = self.item_price * self.item_quantity
-        print(f'{self.item_name} {self.item_quantity} @ {':.2f'.format(self.item_price)} = ${':.2f'.format(item_total_cost)}')
+        print(f'{self.item_name} {self.item_quantity} @ {self.item_price:.2f} = ${item_total_cost:.2f}')
+Item1 = ItemToPurchase('chocolate chips', 3, 1)
+Item1.print_item_cost()
