4c7cac46b4fe01fb92fc341d75459be8142edea4
Jacob Adams <jacob.adams@csuglobal.edu>
2024-08-11 18:24:56 -0500

    class constructor

--- cost_calculator.py
+++ cost_calculator.py
@@ -1,2 +1,5 @@
 class ItemToPurchase:
-    def __init__(self, item_name, item_price, item_quantity):
+    def __init__(self, item_name = 'none', item_price = 0, item_quantity = 0):
+        self.item_name = str(item_name)
+        self.item_price = float(item_price)
+        self.item_quantity = int(item_quantity)
