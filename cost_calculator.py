fd7bf3c532f2b4750f127ae0321f33562e56175c
Jacob Adams <jacob.adams@csuglobal.edu>
2024-08-11 18:39:11 -0500

    First try at method

--- cost_calculator.py
+++ cost_calculator.py
@@ -3,3 +3,6 @@ class ItemToPurchase:
         self.item_name = str(item_name)
         self.item_price = float(item_price)
         self.item_quantity = int(item_quantity)
+    def print_item_cost(self):
+        item_total_cost = self.item_price * self.item_quantity
+        print(f'{self.item_name} {self.item_quantity} @ {':.2f'.format(self.item_price)} = ${':.2f'.format(item_total_cost)}')
