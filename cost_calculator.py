ac6c6466a424343287428c4a575bd524150f2517
Jacob Adams <jacob.adams@csuglobal.edu>
2024-08-11 19:37:32 -0500

    Final

--- cost_calculator.py
+++ cost_calculator.py
@@ -3,8 +3,14 @@ class ItemToPurchase:
         self.item_name = str(item_name)
         self.item_price = float(item_price)
         self.item_quantity = int(item_quantity)
+        self.item_total_cost = self.item_price * self.item_quantity
     def print_item_cost(self):
         item_total_cost = self.item_price * self.item_quantity
-        print(f'{self.item_name} {self.item_quantity} @ {self.item_price:.2f} = ${item_total_cost:.2f}')
+        print(f'{self.item_name} {self.item_quantity} @ ${self.item_price:.2f} = ${self.item_total_cost:.2f}')
+
 Item1 = ItemToPurchase('chocolate chips', 3, 1)
+Item2 = ItemToPurchase('bottled water', 1, 10)
+print('         TOTAL COST:')
 Item1.print_item_cost()
+Item2.print_item_cost()
+print(f'        Total: ${(Item1.item_total_cost + Item2.item_total_cost):.2f}')
